50d1c5cdba0d98f5acab496cecb11ffaf8d6723b
Tarun Verghis <tarun.verghis@gmail.com>
2022-12-01 23:04:00 -0800

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+	echo "Usage: $(basename "$0") <day>" >&2
+	exit 1
+fi
+
+if [ ! -f "./.cookie" ]; then
+	echo "No .cookie file found" >&2
+	exit 1
+fi
+
+if [ ! -d .git ]; then
+	echo "Must be run from the repository root" >&2
+	exit 1
+fi
+
+name="$(printf "aoc%02d" "$1")"
+cookie="$(cat ./.cookie)"
+echo "Creating directory for $name..."
+cargo new --bin "$name"
+echo "Downloading the input for Day $1..."
+curl -H "Cookie: $cookie" https://adventofcode.com/2022/day/"$1"/input > "$name"/input.txt
+echo "Setup complete!"
