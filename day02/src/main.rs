82fa4ad8f7a2d87d5e3e54476f4450397f6fcf24
Tarun Verghis <tarun.verghis@gmail.com>
2022-12-02 01:18:23 -0800

    Day 2 Part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,6 +7,23 @@ enum RoundOutcome {
     Win = 6,
 }
 
+impl RoundOutcome {
+    fn score(&self) -> usize {
+        *self as usize
+    }
+}
+
+impl From<char> for RoundOutcome {
+    fn from(c: char) -> Self {
+        match c {
+            'X' => Self::Loss,
+            'Y' => Self::Draw,
+            'Z' => Self::Win,
+            _ => panic!("unexpected round outcome input {}", c),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Shape {
     Rock = 1,
@@ -19,20 +36,20 @@ impl Shape {
         *self as usize
     }
 
-    fn versus(&self, other: &Shape) -> RoundOutcome {
+    fn play_for_desired_outcome(&self, desired_outcome: RoundOutcome) -> Self {
         use RoundOutcome::{Draw, Loss, Win};
         use Shape::{Paper, Rock, Scissors};
 
-        match (self, other) {
-            (Rock, Rock) => Draw,
-            (Rock, Paper) => Loss,
-            (Rock, Scissors) => Win,
-            (Paper, Rock) => Win,
-            (Paper, Paper) => Draw,
-            (Paper, Scissors) => Loss,
-            (Scissors, Rock) => Loss,
-            (Scissors, Paper) => Win,
-            (Scissors, Scissors) => Draw,
+        match (self, desired_outcome) {
+            (Rock, Loss) => Scissors,
+            (Rock, Draw) => Rock,
+            (Rock, Win) => Paper,
+            (Paper, Loss) => Rock,
+            (Paper, Draw) => Paper,
+            (Paper, Win) => Scissors,
+            (Scissors, Loss) => Paper,
+            (Scissors, Draw) => Scissors,
+            (Scissors, Win) => Rock,
         }
     }
 }
@@ -40,16 +57,16 @@ impl Shape {
 impl From<char> for Shape {
     fn from(x: char) -> Self {
         match x {
-            'A' | 'X' => Shape::Rock,
-            'B' | 'Y' => Shape::Paper,
-            'C' | 'Z' => Shape::Scissors,
+            'A' => Shape::Rock,
+            'B' => Shape::Paper,
+            'C' => Shape::Scissors,
             _ => panic!("unexpected shape input: {}", x),
         }
     }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
-struct Round(Shape, Shape);
+struct Round(Shape, RoundOutcome);
 
 impl From<&str> for Round {
     fn from(line: &str) -> Self {
@@ -61,14 +78,15 @@ impl From<&str> for Round {
 
 impl Round {
     fn score(&self) -> usize {
-        self.1.score() + (self.1.versus(&self.0) as usize)
+        let my_shape = self.0.play_for_desired_outcome(self.1);
+        my_shape.score() + self.1.score()
     }
 }
 
 fn main() {
     let rounds = parse_input(INPUT);
 
-    println!("Part 1: {}", total_score(&rounds));
+    println!("Total score: {}", total_score(&rounds));
 }
 
 fn parse_input(input: &str) -> Vec<Round> {
@@ -81,6 +99,7 @@ fn total_score(rounds: &[Round]) -> usize {
 
 #[cfg(test)]
 mod test {
+    use crate::RoundOutcome::{Draw, Loss, Win};
     use crate::Shape::{Paper, Rock, Scissors};
     use crate::{parse_input, total_score, Round};
 
@@ -93,16 +112,12 @@ C Z"#;
         let rounds: Vec<Round> = parse_input(SAMPLE_INPUT);
         assert_eq!(
             rounds,
-            vec![
-                Round(Rock, Paper),
-                Round(Paper, Rock),
-                Round(Scissors, Scissors)
-            ]
+            vec![Round(Rock, Draw), Round(Paper, Loss), Round(Scissors, Win)]
         );
     }
 
     #[test]
     fn test_total_score() {
-        assert_eq!(total_score(&parse_input(SAMPLE_INPUT)), 15);
+        assert_eq!(total_score(&parse_input(SAMPLE_INPUT)), 12);
     }
 }
